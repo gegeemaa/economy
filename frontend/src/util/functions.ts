2796b6c99e040a949728ae7b49cd2854f156f8de
gma <gerelmaaskh@gmail.com>
2024-04-06 21:34:31 +0200

    Change back-end URL

--- frontend/src/util/functions.ts
+++ frontend/src/util/functions.ts
@@ -12,4 +12,4 @@ export const getCurrentYear = () => {
 };
 
 // export const API_URL = import.meta.env.VITE_API_URL;
-export const API_URL = "https://trelleborg.mnsushi.se";
+export const API_URL = "https://trelleborg.mnsushi.se/";
