022bf917874ca8012dfb5efd9793191014e58670
gma <gerelmaaskh@gmail.com>
2024-04-11 11:48:24 +0200

    Login page ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./MainLayout";
+import Login from "./components/auth/Login";
 
 const App: React.FC = () => {
   return (
--- frontend/src/MainLayout.tsx
+++ frontend/src/MainLayout.tsx
@@ -1,27 +1,15 @@
-import React, { useState } from "react";
-import { AccountBookOutlined } from "@ant-design/icons";
-import { Button, Layout, Menu, Popover, theme } from "antd";
+import { useState } from "react";
+import { Button, Layout, Popover } from "antd";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import type { MenuProps } from "antd";
-import FakturaTable from "./components/FakturaTable";
 import styled from "styled-components";
-import { useSelector } from "react-redux";
 import iconProfile from "./util/icon-profile.svg";
 import Tooltip from "./common/tooltip";
 import { BASE_COLOR } from "./common/index";
+import Login from "./components/auth/Login";
+import Body from "./components/layout/Body";
 
-const { Content, Sider, Header } = Layout;
+const { Header } = Layout;
 
-const menuDataOriginal = [
-  {
-    label: "Faktura",
-    icon: AccountBookOutlined,
-    children: [
-      // { label: "Incoming", linkcomponent: "/incomingInvoice" },
-      { label: "Outgoing", linkcomponent: "/outgoingInvoice" },
-    ],
-  },
-];
 export const FlexRow = styled.div`
   display: flex;
   flex-direction: row;
@@ -47,36 +35,13 @@ export const HeaderStyled = styled(Header)`
   padding-right: 20;
 `;
 
-const menuData = menuDataOriginal.map((item, index) => {
-  const key = String(index + 1);
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(item.icon),
-    label: item.label,
-    children: item.children.map((subitem) => {
-      return {
-        key: subitem.linkcomponent,
-        label: subitem.label,
-      };
-    }),
-  };
-});
-
 const MainLayout = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
   const navigate = useNavigate();
-
-  const onClick: MenuProps["onClick"] = (e) => {
-    navigate(e.key);
-  };
 
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   // const authState = useSelector((state: RootState) => state.auth);
   // const { name, profileUrl, isAuthenticated };
-  const isAuthenticated = true;
+  const isAuthenticated = false;
   const profileUrl = "";
 
   const onClickLogin = () => {
@@ -110,43 +75,10 @@ const MainLayout = () => {
           </Popover>
         </Tooltip>
       </HeaderStyled>
-      <Layout>
-        <Sider
-          width={200}
-          style={{ background: colorBgContainer }}
-          breakpoint="sm"
-          collapsedWidth="0"
-        >
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100%", borderRight: 0 }}
-            onClick={onClick}
-            items={menuData}
-          />
-        </Sider>
-        <Layout style={{ padding: "24px" }}>
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            <Routes>
-              <Route index element={<FakturaTable />} />
-              {/* <Route
-                path="/incomingInvoice"
-                element={<IncomingFakturaTable />}
-              /> */}
-              <Route path="/outgoingInvoice" element={<FakturaTable />} />
-            </Routes>
-          </Content>
-        </Layout>
-      </Layout>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/body" element={<Body />} />
+      </Routes>
     </Layout>
   );
 };
