a396b8c063ccf7b6cf382ee890f6e5d4bd221e65
gma <gerelmaaskh@gmail.com>
2024-04-25 13:54:01 +0200

    Add theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MainLayout from "./MainLayout";
+import { ConfigProvider } from "antd";
+import "./index.css";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <MainLayout />
+      <ConfigProvider
+        theme={{
+          token: {
+            // Seed Token
+            colorPrimary: "#00b96b",
+            borderRadius: 2,
+            colorLink: "#00b96b",
+            // Alias Token
+            colorBgContainer: "#f6ffed",
+          },
+        }}
+      >
+        <MainLayout />
+      </ConfigProvider>
     </BrowserRouter>
   );
 };
